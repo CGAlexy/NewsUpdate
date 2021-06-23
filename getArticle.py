a8433898e06967114fbd80c87156862c190d4d7d
CGAlexy <cgalexy1@gmail.com>
2021-06-22 20:48:38 -0400

    takes input and gets articles from past week

--- getArticle.py
+++ getArticle.py
@@ -4,16 +4,31 @@
     #https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=API_KEY
 from testText import *
 import requests
+from datetime import datetime, timedelta
 
-#takes in topic and turns it into the appropriot query for the api
-topic = "Senate"
-q = "&q="+ topic
-api_key = '33d4079bbea841fcaf722a3206624a35'
-articles = requests.get('https://newsapi.org/v2/top-headlines?country=us'+q+'&apiKey='+api_key)
-articles_json = articles.json()
-body = ''
-for art in articles_json['articles']: 
-    body = body + '\n' +art['url']
-print(body)
 
-email_alert(topic, body, "cgalexy1@gmail.com")
+def get_articles(topic, to):
+    #set query for the topic
+    q = "&q="+ topic
+    #Finds date from one week ago to use as start date for search
+    today = datetime.today()
+    start_date = today-timedelta(days = 7)
+    start_date_string = "from = "+start_date.strftime("%m/%d/%Y")
+    #key to get access to api
+    api_key = '33d4079bbea841fcaf722a3206624a35'
+
+    #call GET request with api url
+    articles = requests.get('https://newsapi.org/v2/everything?'+start_date_string+q+'&apiKey='+api_key)
+    articles_json = articles.json()
+    #create body to send in email
+    body = ''
+    #extract urls of each article and add them to the body
+    for art in articles_json['articles']: 
+        body = body + '\n\n' +art['url']
+    #send email with body
+    email_alert(topic, body, to)
+
+if __name__ == '__main__':
+        topic = input("Enter Topic to Search: ")
+        get_articles(topic, "cgalexy1@gmail.com")
+        #get_articles(topic, "2672709152@vtext.com") 
